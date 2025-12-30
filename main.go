964c4ac57eb474dfa711c780c4ab623eea1ae611
cn1095 <1095053105@qq.com>
2025-12-30 21:03:26 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3662,15 +3662,35 @@ func initializeIPDatabases() {
 	// log.Println("IP数据库异步初始化完成")
 }
 
-func runAsDaemon() {  
-    switch runtime.GOOS {  
-    case "linux", "freebsd", "darwin":  
-        runAsDaemonUnix()  
-    case "windows":  
-        runAsDaemonWindows()  
-    default:  
-        log.Println("当前系统不支持后台模式")  
-    }  
+func runAsDaemon() {
+	switch runtime.GOOS {
+	case "linux", "freebsd", "darwin":
+		if os.Getppid() != 1 {
+			cmd := exec.Command(os.Args[0], os.Args[1:]...)
+			cmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
+			cmd.Stdout, cmd.Stderr, cmd.Stdin = nil, nil, nil
+			
+			// 显式传递环境变量  
+            cmd.Env = os.Environ()
+			err := cmd.Start()
+			if err != nil {
+				log.Fatalf("后台运行失败: %v", err)
+			}
+			os.Exit(0)
+		}
+
+	case "windows":
+		cmd := exec.Command(os.Args[0], os.Args[1:]...)
+		cmd.Env = os.Environ()
+		err := cmd.Start()
+		if err != nil {
+			log.Fatalf("后台运行失败: %v", err)
+		}
+		os.Exit(0)
+
+	default:
+		log.Println("当前系统不支持后台模式")
+	}
 }
 
 func main() {
